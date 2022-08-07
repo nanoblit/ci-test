ca717c373a9dfdd366ac46791835488578d1bbcd
nanoblit <jakub.k.maleta@gmail.com>
2022-08-07 11:57:42 +0200

    Add COOL

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Test/i);
+  const linkElement = screen.getByText(/Cool/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test
+          Cool
         </a>
       </header>
     </div>
